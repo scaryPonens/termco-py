82482cb0d0bd5764b72961d30d2d9cf52054562c
Reuben Peter-Paul <reuben.peterpaul@gmail.com>
2025-08-17 14:36:21 -0300

    Fix URL in __init__.py for termcopy project

--- src/termcopy/__init__.py
+++ src/termcopy/__init__.py
@@ -4,4 +4,4 @@ termcopy - A CLI tool for terminal clipboard operations
 
 __version__ = "0.1.0"
 __author__ = "scaryPonens"
-__url__ = "https://github.com/scaryPonens/termcopy-py"
+__url__ = "https://github.com/scaryPonens/termco-py"
