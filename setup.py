5047269fd4c1624df6e02183eccd9efa2ac8e584
Blueshell <blueshell@local>
2026-03-04 04:09:13 +0000

    Bump package version to 0.2.1 for PyPI publish

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="termcopy",
-    version="0.1.0",
+    version="0.2.1",
     author="scaryPonens",
     author_email="reuben@reubenpeterpaul.com",
     description="A CLI tool for terminal clipboard operations",
